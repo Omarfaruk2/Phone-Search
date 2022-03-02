79f89d66ac9f74dc5a2c512fc9a830b81bbb2b88
Omarfaruk4200 <emon35293@gmail.com>
2022-03-02 10:48:05 +0600

    added last

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ const loadMobile = () => {
             .then(res => res.json())
             .then(data => {
                 // console.log(data.data)
-                if (inputValue.toLowerCase() === 'nova' || inputValue.toLowerCase() === 'huawei' || inputValue.toLowerCase() === 'samsung'
-                    || inputValue.toLowerCase() === 'iphone' || inputValue.toLowerCase() === 'tab' || inputValue.toLowerCase() === 'oppo' || inputValue.toLowerCase() === '10') {
-                    // console.log(data.data.length)
+                if (inputValue.toLowerCase() === 'nova' || inputValue.toLowerCase() === 'huawei' || inputValue.toLowerCase() === 'samsung' || inputValue.toLowerCase() === "galaxy"
+                    || inputValue.toLowerCase() === 'iphone' || inputValue.toLowerCase() === 'tab' || inputValue.toLowerCase() === 'oppo' || inputValue.toLowerCase() === '10' || inputValue.toLowerCase() === 'apple' || inputValue.toLowerCase() === 'watch' || inputValue.toLowerCase() === 'find') {
+
                     displayMobile((data.data).slice(0, 21))
                 }
                 else {
@@ -43,7 +43,7 @@ const displayMobile = (phones) => {
         div.innerHTML = `
         <div class="col-lg-4">
             <div class="card px-4 pt-3 mb-4 check-border shadow-lg" style="width: 21rem;">
-                <img class="card-img-top w-75 b-0 mx-auto" src="${phone.image}" alt="Card image cap">
+                <img class="card-img-top w-50 b-0 mx-auto" src="${phone.image}" alt="Card image cap">
                 <div class="card-body w-100">
                     <h6 class="card-title text-center">Mobile: ${phone.phone_name}</h6>
                     <h6 class="card-title text-center">Divice: ${phone.brand}</h6>
@@ -67,91 +67,29 @@ const loadDetails = (id) => {
 // -------------------innerHTML for details-----------------------
 const setDetails = (info) => {
     const parent = document.getElementById("details-mobile")
-    if (info.others == undefined) {
-        parent.innerHTML = `
-        <div class="card mx-auto" style="width: 45rem;">
+    parent.innerHTML = `
+        <div class="card mx-auto" style="width: 65rem;">
             <h3 class="card-title text-center mt-3">Brand New ${info.name}</h3>
             <img src="${info.image}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body">
                 <h4 class="card-title fw-bold fw-bold">Full Specifications Of ${info.name} </h4>
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item table-hover">ReleaseDate :${info.releaseDate}</li>
+                <li class="list-group-item table-hover">ReleaseDate : ${info.releaseDate ? info.releaseDate : "Date Not Published"}</li>
                 <h5 class="list-group-item bg-light table-hover text-success">MainFeatures :</h5>
                 <li class="list-group-item">Storage: ${info.mainFeatures.storage}</li>
                 <li class="list-group-item bg-light">DisplaySize: ${info.mainFeatures.displaySize}</li>
                 <li class="list-group-item">ChipSet: ${info.mainFeatures.chipSet}</li>
                 <li class="list-group-item bg-light">Memory: ${info.mainFeatures.memory}</li>
-                <h5 class="list-group-item fw-bold">Sensors: ${info.mainFeatures.sensors}</h5>         
+                <h6 class="list-group-item fw-bold">Sensors: ${info.mainFeatures.sensors}</h6>         
                 <li class="list-group-item bg-light">Brand: ${info.brand}</li>
                 <h5 class="list-group-item text-success">Other Features:</h5>
-                <li class="list-group-item ">Bluetooth: Result Not Avilable</li>
-                <li class="list-group-item bg-light">GPS: Result Not Avilable</li>
-                <li class="list-group-item ">NFC: Result Not Avilable</li>
-                <li class="list-group-item bg-light">Radio: Result Not Avilable</li>
-                <li class="list-group-item ">USB: Result Not Avilable</li>
+                <li class="list-group-item ">Bluetooth: ${info?.others?.NFC ?? "Result Not Avilable"}</li>
+                <li class="list-group-item bg-light">GPS: ${info?.others?.NFC ?? "Result Not Avilable"}</li>
+                <li class="list-group-item ">NFC: ${info?.others?.NFC ?? "Result Not Avilable"}</li>
+                <li class="list-group-item bg-light">Radio: ${info?.others?.NFC ?? "Result Not Avilable"}</li>
+                <li class="list-group-item ">USB: ${info?.others?.NFC ?? "Result Not Avilable"}</li>
             </ul>
         </div>
     `
-    }
-    else if (info.releaseDate === '') {
-        parent.innerHTML = `
-        <div class="card mx-auto" style="width: 45rem;">
-            <h3 class="card-title text-center mt-3">Brand New ${info.name}</h3>
-            <img src="${info.image}" class="card-img-top w-50 mx-auto" alt="...">
-            <div class="card-body">
-                <h4 class="card-title fw-bold">Full Specifications Of ${info.name} </h4>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ReleaseDate :<h6 class="d-inline"> Comming soon</h6></li>
-                <h5 class="list-group-item bg-light text-success">MainFeatures :</h5>
-                <li class="list-group-item table-hover">Storage: ${info.mainFeatures.storage}</li>
-                <li class="list-group-item bg-light table-hover">DisplaySize: ${info.mainFeatures.displaySize}</li>
-                <li class="list-group-item">ChipSet: ${info.mainFeatures.chipSet}</li>
-                <li class="list-group-item bg-light">Memory: ${info.mainFeatures.memory}</li>
-                <h5 class="list-group-item fw-bold">Sensors: ${info.mainFeatures.sensors}</h5>         
-                <li class="list-group-item bg-light">Brand: ${info.brand}</li>
-                <h5 class="list-group-item text-success">Other Features:</h5>
-                <li class="list-group-item ">Bluetooth: ${info.others.Bluetooth}</li>
-                <li class="list-group-item bg-light">GPS: ${info.others.GPS}</li>
-                <li class="list-group-item ">NFC: ${info.others.NFC}</li>
-                <li class="list-group-item bg-light">Radio: ${info.others.Radio}</li>
-                <li class="list-group-item ">USB: ${info.others.USB}</li>
-            </ul>
-        </div>
-    `
-    }
-    else {
-        parent.innerHTML = `
-        <div class="card mx-auto" style="width: 45rem;">
-            <h3 class="card-title text-center mt-3">Brand New ${info.name}</h3>
-            <img src="${info.image}" class="card-img-top w-50 mx-auto" alt="...">
-            <div class="card-body">
-                <h4 class="card-title fw-bold fw-bold">Full Specifications Of ${info.name} </h4>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ReleaseDate :${info.releaseDate}</li>
-                <h4 class="list-group-item bg-light text-success fw-bold">MainFeatures :</h4>
-                <li class="list-group-item">Storage: ${info.mainFeatures.storage}</li>
-                <li class="list-group-item bg-light">DisplaySize: ${info.mainFeatures.displaySize}</li>
-                <li class="list-group-item">ChipSet: ${info.mainFeatures.chipSet}</li>
-                <li class="list-group-item bg-light">Memory: ${info.mainFeatures.memory}</li>
-                <h5 class="list-group-item fw-bold">Sensors: ${info.mainFeatures.sensors}</h5>         
-                <li class="list-group-item bg-light">Brand: ${info.brand}</li>
-                <h4 class="list-group-item text-success fw-bold">Other Features:</h4> 
-                <li class="list-group-item ">Bluetooth: ${info.others.Bluetooth}</li>
-                <li class="list-group-item bg-light">GPS: ${info.others.GPS}</li>
-                <li class="list-group-item ">NFC: ${info.others.NFC}</li>
-                <li class="list-group-item bg-light">Radio: ${info.others.Radio}</li>
-                <li class="list-group-item ">USB: ${info.others.USB}</li>
-            </ul>
-        </div>
-    `
-    }
 }
-
-
-
-
-
-
